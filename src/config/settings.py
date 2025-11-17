8dcd6ab514237e2505472c581a4327cdc69b8efd
Paulo Fernando <paulo.fdo@hotmail.com>
2025-11-17 19:43:19 -0300

    feat: Add config module (#3)
    * Add settings module and replace main.py references
    
    * Move main.py to src folder

---
+++ src/config/settings.py
@@ -0,0 +1,7 @@
+# src/config/settings.py
+import yaml
+
+def carregar_config(path: str = "config/settings.yaml") -> dict:
+    """Carrega um arquivo de configuração YAML."""
+    with open(path, 'r') as file:
+        return yaml.safe_load(file)
