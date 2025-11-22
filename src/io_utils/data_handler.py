9da2e1e414dfe706cf8aeec984539f16ecc44b83
Paulo Fernando <paulo.fdo@hotmail.com>
2025-11-22 00:17:23 -0300

    Add try/catch on DataHandler

--- src/io_utils/data_handler.py
+++ src/io_utils/data_handler.py
@@ -2,6 +2,10 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import (StructType, StructField, StringType, LongType, 
                                ArrayType, DateType, FloatType, TimestampType, BooleanType)
+from pyspark.errors import AnalysisException 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class DataHandler:
     """
@@ -9,6 +13,10 @@ class DataHandler:
     """
 
     def __init__(self, spark: SparkSession):
+        """Inicializa o DataHandler com uma sessão Spark.
+        :param spark: Instância da SparkSession.
+        """
+        
         self.spark = spark
 
     def _get_schema_pedidos(self) -> StructType:
@@ -45,23 +53,29 @@ class DataHandler:
         """Carrega o dataframe de pedidos a partir de arquivos CSV comprimidos.
         :param path: Caminho para os arquivos de pedidos."""
         schema = self._get_schema_pedidos()
-        df = self.spark.read.option("compression", compression).csv(path, header=header, schema=schema, sep=separator)
-        
-        df = (df.withColumnRenamed("ID_PEDIDO", "id_pedido")
-            .withColumnRenamed("PRODUTO", "produto")
-            .withColumnRenamed("VALOR_UNITARIO", "valor_unitario")
-            .withColumnRenamed("QUANTIDADE", "quantidade")
-            .withColumnRenamed("DATA_CRIACAO", "data_criacao")
-            .withColumnRenamed("UF", "uf")
-            .withColumnRenamed("ID_CLIENTE", "id_cliente")
-        )
 
-        return df
+        try:
+            df = self.spark.read.option("compression", compression).csv(path, header=header, schema=schema, sep=separator)        
+            return (df.withColumnRenamed("ID_PEDIDO", "id_pedido")
+                .withColumnRenamed("PRODUTO", "produto")
+                .withColumnRenamed("VALOR_UNITARIO", "valor_unitario")
+                .withColumnRenamed("QUANTIDADE", "quantidade")
+                .withColumnRenamed("DATA_CRIACAO", "data_criacao")
+                .withColumnRenamed("UF", "uf")
+                .withColumnRenamed("ID_CLIENTE", "id_cliente")
+            )  
+        except Exception as e:
+            logger.error(f"Problemas ao carregar dados de pedidos: {e}")
+            return
     
     def load_pagamentos(self, path: str) -> DataFrame:
         """Carrega o dataframe de pagamentos a partir de arquivos JSON comprimidos.
         :param path: Caminho para os arquivos de pagamentos.
         """
         schema = self._get_schema_pagamentos()
-        return self.spark.read.option("compression", "gzip").json(path, schema=schema)
+        try:
+            return self.spark.read.option("compression", "gzip").json(path, schema=schema)
+        except Exception as e:
+            logger.error(f"Problemas ao carregar dados de pagamentos: {e}")
+            return
 
