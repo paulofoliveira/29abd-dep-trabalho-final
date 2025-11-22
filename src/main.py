cd223261559fde84f368bea65f989497f0259c4b
Paulo Fernando <paulo.fdo@hotmail.com>
2025-11-21 23:32:08 -0300

    Add main method on main.py

--- src/main.py
+++ src/main.py
@@ -4,35 +4,40 @@ from io_utils.data_handler import DataHandler
 from session.spark_session import SparkSessionManager
 from processing.transformations import Transformation
 
-config = carregar_config()
-app_name = config['spark']['app_name']
-spark = SparkSessionManager.get_spark_session(app_name)
-data_handler = DataHandler(spark)
-transformation = Transformation()
+def main():
 
-print("Abrindo o dataframe de pedidos")
-path_pedidos = config['paths']['pedidos']
-compression_pedidos = config['file_options']['pedidos_csv']['compression']
-header_pedidos = config['file_options']['pedidos_csv']['header']
-separator_pedidos = config['file_options']['pedidos_csv']['sep']
+    config = carregar_config()
+    app_name = config['spark']['app_name']
+    spark = SparkSessionManager.get_spark_session(app_name)
+    data_handler = DataHandler(spark)
+    transformation = Transformation()
 
-pedidos_df = data_handler.load_pedidos(path_pedidos, compression_pedidos, header_pedidos, separator_pedidos)
- 
-pedidos_df.show(5, truncate=False)
+    print("Abrindo o dataframe de pedidos")
+    path_pedidos = config['paths']['pedidos']
+    compression_pedidos = config['file_options']['pedidos_csv']['compression']
+    header_pedidos = config['file_options']['pedidos_csv']['header']
+    separator_pedidos = config['file_options']['pedidos_csv']['sep']
 
-print("Abrindo o dataframe de pagamentos")
-path_pagamentos = config['paths']['pagamentos']
-pagamentos_df = data_handler.load_pagamentos(path_pagamentos)
+    pedidos_df = data_handler.load_pedidos(path_pedidos, compression_pedidos, header_pedidos, separator_pedidos)
+    
+    pedidos_df.show(5, truncate=False)
 
-pagamentos_df.show(5, truncate=False)
+    print("Abrindo o dataframe de pagamentos")
+    path_pagamentos = config['paths']['pagamentos']
+    pagamentos_df = data_handler.load_pagamentos(path_pagamentos)
 
-print("Monta resultado filtrando e selecionando colunas a partir dos dataframes de pedidos e pagamentos")
-filtrado_df = transformation.calculate(pedidos_df, pagamentos_df)
+    pagamentos_df.show(5, truncate=False)
 
-filtrado_df.show(20, truncate=False)
+    print("Monta resultado filtrando e selecionando colunas a partir dos dataframes de pedidos e pagamentos")
+    filtrado_df = transformation.calculate(pedidos_df, pagamentos_df)
 
-print("Escrevendo o resultado em parquet")
-path_output = config['paths']['output']
-filtrado_df.write.mode("overwrite").parquet(path_output)
+    filtrado_df.show(20, truncate=False)
 
-spark.stop()
+    print("Escrevendo o resultado em parquet")
+    path_output = config['paths']['output']
+    filtrado_df.write.mode("overwrite").parquet(path_output)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
