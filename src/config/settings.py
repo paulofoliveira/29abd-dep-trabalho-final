038e759a6ab4b8bf59be381ffe699bdb13e5e3f8
Paulo Fernando <paulo.fdo@hotmail.com>
2025-11-21 23:50:35 -0300

    Add comment on carregar_config method from settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,6 +2,7 @@
 import yaml
 
 def carregar_config(path: str = "config/settings.yml") -> dict:
-    """Carrega um arquivo de configuração YAML."""
+    """Carrega um arquivo de configuração YAML.
+    :param path: Caminho para o arquivo de configuração YAML."""
     with open(path, 'r') as file:
         return yaml.safe_load(file)
