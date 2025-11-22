7f73af31e3c94198bae3b4860e9e84f1bc663284
Paulo Fernando <paulo.fdo@hotmail.com>
2025-11-21 23:44:00 -0300

    Fix run method from pipeline on main.py

--- src/main.py
+++ src/main.py
@@ -3,39 +3,22 @@ from config.settings import carregar_config
 from io_utils.data_handler import DataHandler
 from session.spark_session import SparkSessionManager
 from processing.transformations import Transformation
+from pipeline.pipeline import Pipeline
 
 def main():
-
+    """
+    Função principal que atua como a "Raiz de Composição".
+    Configura e executa o pipeline.
+    """
+    
     config = carregar_config()
     app_name = config['spark']['app_name']
     spark = SparkSessionManager.get_spark_session(app_name)
     data_handler = DataHandler(spark)
     transformation = Transformation()
 
-    print("Abrindo o dataframe de pedidos")
-    path_pedidos = config['paths']['pedidos']
-    compression_pedidos = config['file_options']['pedidos_csv']['compression']
-    header_pedidos = config['file_options']['pedidos_csv']['header']
-    separator_pedidos = config['file_options']['pedidos_csv']['sep']
-
-    pedidos_df = data_handler.load_pedidos(path_pedidos, compression_pedidos, header_pedidos, separator_pedidos)
-    
-    pedidos_df.show(5, truncate=False)
-
-    print("Abrindo o dataframe de pagamentos")
-    path_pagamentos = config['paths']['pagamentos']
-    pagamentos_df = data_handler.load_pagamentos(path_pagamentos)
-
-    pagamentos_df.show(5, truncate=False)
-
-    print("Monta resultado filtrando e selecionando colunas a partir dos dataframes de pedidos e pagamentos")
-    filtrado_df = transformation.calculate(pedidos_df, pagamentos_df)
-
-    filtrado_df.show(20, truncate=False)
-
-    print("Escrevendo o resultado em parquet")
-    path_output = config['paths']['output']
-    filtrado_df.write.mode("overwrite").parquet(path_output)
+    pipeline = Pipeline(spark, data_handler, transformation, config)
+    pipeline.run()
 
     spark.stop()
 
---
+++ src/pipeline/pipeline.py
@@ -0,0 +1,38 @@
+# src/pipeline/pipeline.py
+
+class Pipeline:
+    """
+    Classe que orquestra a execução do pipeline de dados.
+    """
+    def __init__(self, spark, data_handler, transformation, config):
+
+        self.spark = spark
+        self.data_handler = data_handler
+        self.transformation = transformation
+        self.config = config
+
+    def run(self):
+        print("Abrindo o dataframe de pedidos")
+        path_pedidos = self.config['paths']['pedidos']
+        compression_pedidos = self.config['file_options']['pedidos_csv']['compression']
+        header_pedidos = self.config['file_options']['pedidos_csv']['header']
+        separator_pedidos = self.config['file_options']['pedidos_csv']['sep']
+
+        pedidos_df = self.data_handler.load_pedidos(path_pedidos, compression_pedidos, header_pedidos, separator_pedidos)
+        
+        pedidos_df.show(5, truncate=False)
+
+        print("Abrindo o dataframe de pagamentos")
+        path_pagamentos = self.config['paths']['pagamentos']
+        pagamentos_df = self.data_handler.load_pagamentos(path_pagamentos)
+
+        pagamentos_df.show(5, truncate=False)
+
+        print("Monta resultado filtrando e selecionando colunas a partir dos dataframes de pedidos e pagamentos")
+        resultado_df = self.transformation.calculate(pedidos_df, pagamentos_df)
+
+        resultado_df.show(20, truncate=False)
+
+        print("Escrevendo o resultado em parquet")
+        path_output = self.config['paths']['output']
+        resultado_df.write.mode("overwrite").parquet(path_output)
